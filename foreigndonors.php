1acb5d9aee744a65ebe58d08a389b3fca29f963d
John Twyman <john.twyman@greens.org.au>
2019-03-05 12:16:14 +1100

    Change language

--- foreigndonors.php
+++ foreigndonors.php
@@ -189,8 +189,8 @@ function foreigndonors_civicrm_buildForm($formName, &$form) {
     }
 
     // Add the checkbox to the public form
-    $form->addElement('checkbox', 'foreigndonor', ts('I affirm I am not a foreign donor'));
-    $form->addRule('foreigndonor', ts('You must affirm you are not a foreign donor'), 'required', NULL, 'client');
+    $form->addElement('checkbox', 'foreigndonor', ts('I am an Australian Citizen or Permanent Resident'));
+    $form->addRule('foreigndonor', ts('You must affirm you are an Australian Citizen or Permanent Resident'), 'required', NULL, 'client');
     $templatePath = realpath(dirname(__FILE__) . '/templates');
     CRM_Core_Region::instance('form-bottom')->add(array(
       'template' => "{$templatePath}/foreigndonors.tpl",
--- templates/foreigndonors.tpl
+++ templates/foreigndonors.tpl
@@ -1,6 +1,6 @@
 {* template block that contains the foreign donor affirmation field *}
 <div id="foreigndonor">
   <div>{$form.foreigndonor.html}</div>
-  <div>I affirm I am not a foreign donor</div>
+  <div>I am an Australian Citizen or Permanent Resident</div>
 </div>
 {* reposition block after #otherBlock *}
