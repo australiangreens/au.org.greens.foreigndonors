d8cae290f796a7bbd8a952926fa9a08c3415356d
Seamus Lee <seamuslee001@gmail.com>
2019-11-26 15:20:13 +1100

    Fix javascript error

--- js/event-form.js
+++ js/event-form.js
@@ -2,8 +2,9 @@
   $('#billing-payment-block').on('crmLoad', function() {
     var paymentFieldsDiv = $('#payment_information');
     paymentFieldsDiv.append('<div id="foreigndonor"><div><input id="foreigndonor" name="foreigndonor" type="checkbox" value="1" class="crm-form-checkbox required"></div><div>' + CRM.vars.foreigndonors.label + '</div></div>');
-   if (CRM.vars.foreigndonors.domainid == '8') {
-     paymentFieldsDiv.append('<div id="prohibiteddonor"><div><input id="prohibiteddonor" name="prohibiteddonor" type="checkbox" value="1" class="crm-form-checkbox required"></div><div>You must affirm you are not a prohibited donor as per NSW Electoral legislation</div></div>'
+    if (CRM.vars.foreigndonors.domainid == '8') {
+      paymentFieldsDiv.append('<div id="prohibiteddonor"><div><input id="prohibiteddonor" name="prohibiteddonor" type="checkbox" value="1" class="crm-form-checkbox required"></div><div>You must affirm you are not a prohibited donor as per NSW Electoral legislation</div></div>');
+    }
   });
   var paymentFieldsDiv = $('#payment_information');
     paymentFieldsDiv.append('<div id="foreigndonor"><div><input id="foreigndonor" name="foreigndonor" type="checkbox" value="1" class="crm-form-checkbox required"></div><div>' + CRM.vars.foreigndonors.label + '</div></div>');
